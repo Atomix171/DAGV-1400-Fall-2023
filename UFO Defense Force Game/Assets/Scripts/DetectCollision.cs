526f17fa9f9bd25191758be6d583d1b35df282d2
Vryan Arias <vryanarias@gmail.com>
2023-11-08 20:51:23 -0700

    Added Music and Particle effect

--- UFO Defense Force Game/Assets/Scripts/DetectCollision.cs
+++ UFO Defense Force Game/Assets/Scripts/DetectCollision.cs
@@ -8,6 +8,8 @@ public class DetectCollision : MonoBehaviour
 
     public int scoreToGive;
 
+    public ParticleSystem explosionParticle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +18,16 @@ public class DetectCollision : MonoBehaviour
 
     void OnTriggerEnter(Collider other) // once the trigger has been entered record collision in the argument variable "other"
     {
+        Explosion();
         scoreManager.IncreaseScore(scoreToGive); // increase the score
         Destroy(gameObject);
         Destroy(other.gameObject);
+        
+    }
+
+    void Explosion()
+    {
+        Instantiate(explosionParticle,transform.position, transform.rotation);
     }
 
     
